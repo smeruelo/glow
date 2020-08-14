c4fe424125f6391fde8a4e5390bfc287ec0f1cc6
Sonia Meruelo <smeruelo@gmail.com>
2020-08-14 23:36:34 +0200

    Add dummy backend that serves a project list

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smeruelo/glow
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Project struct {
+	Name     string
+	Goal     int
+	Achieved int
+}
+
+var projects = []Project{
+	{Name: "First", Goal: 3000, Achieved: 1030},
+	{Name: "Second", Goal: 700, Achieved: 0},
+	{Name: "Third", Goal: 5550, Achieved: 2200},
+}
+
+func main() {
+	http.HandleFunc("/projects", func(w http.ResponseWriter, r *http.Request) {
+		b, err := json.Marshal(projects)
+		if err != nil {
+			log.Printf("Unable to marshal projects: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Write(b)
+	})
+
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
