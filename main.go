e3bdd6286083b40be0213202588a940996c14a2c
Sonia Meruelo <smeruelo@gmail.com>
2020-08-16 18:51:03 +0200

    Read projects from DB instead of faking it

--- main.go
+++ main.go
@@ -15,12 +15,6 @@ type Project struct {
 	Achieved int
 }
 
-var projects = []Project{
-	{Name: "First", Goal: 3000, Achieved: 1030},
-	{Name: "Second", Goal: 700, Achieved: 0},
-	{Name: "Third", Goal: 5550, Achieved: 2200},
-}
-
 func main() {
 	dbHost, ok := os.LookupEnv("DB_HOST")
 	if !ok {
@@ -39,14 +33,31 @@ func main() {
 	defer c.Close()
 
 	http.HandleFunc("/projects", func(w http.ResponseWriter, r *http.Request) {
-		b, err := json.Marshal(projects)
+		projectsJSON, err := redis.StringMap(c.Do("HGETALL", "projects"))
 		if err != nil {
-			log.Printf("Unable to marshal projects: %s", err)
+			log.Printf("Database error: %s", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+
+		projects := make([]Project, len(projectsJSON))
+		i := 0
+		for _, pJSON := range projectsJSON {
+			var p Project
+			if err = json.Unmarshal([]byte(pJSON), &p); err != nil {
+				log.Printf("Unable to unmarshal project: %s", err)
+			} else {
+				projects[i] = p
+				i++
+			}
+		}
+
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.Write(b)
+		output, err := json.Marshal(projects)
+		if err != nil {
+			log.Printf("Unable to marshal projects: %s", err)
+		}
+		w.Write(output)
 	})
 
 	log.Fatal(http.ListenAndServe(":80", nil))
