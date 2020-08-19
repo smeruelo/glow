5a8cf022b66c8db3b88e48b977dca74d9f29c993
Sonia Meruelo <smeruelo@gmail.com>
2020-08-19 22:36:53 +0200

    Remove the old REST-style projects handler

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -9,29 +8,9 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gomodule/redigo/redis"
-	"github.com/google/uuid"
 	"github.com/smeruelo/glow/graph"
 	"github.com/smeruelo/glow/graph/generated"
 )
-
-type Project struct {
-	Name     string `json:"Name"`
-	Goal     int    `json:"Goal"`
-	Achieved int    `json:"Achieved"`
-}
-
-func fetchProjectByID(c redis.Conn, uuid uuid.UUID) (Project, bool) {
-	pJSON, err := redis.Bytes(c.Do("HGET", "projects", uuid.String()))
-	if err != nil {
-		return Project{}, false
-	}
-	var p Project
-	if err := json.Unmarshal(pJSON, &p); err != nil {
-		log.Printf("Unable to unmarshal project: %s", err)
-		return Project{}, false
-	}
-	return p, true
-}
 
 func main() {
 	dbHost, ok := os.LookupEnv("DB_HOST")
@@ -49,34 +28,6 @@ func main() {
 		log.Printf("Unable to connect to database: %s", err)
 	}
 	defer db.Close()
-
-	http.HandleFunc("/projects", func(w http.ResponseWriter, r *http.Request) {
-		projectsJSON, err := redis.StringMap(db.Do("HGETALL", "projects"))
-		if err != nil {
-			log.Printf("Database error: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		projects := make([]Project, len(projectsJSON))
-		i := 0
-		for _, pJSON := range projectsJSON {
-			var p Project
-			if err := json.Unmarshal([]byte(pJSON), &p); err != nil {
-				log.Printf("Unable to unmarshal project: %s", err)
-			} else {
-				projects[i] = p
-				i++
-			}
-		}
-
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		output, err := json.Marshal(projects)
-		if err != nil {
-			log.Printf("Unable to marshal projects: %s", err)
-		}
-		w.Write(output)
-	})
 
 	graphqlServer := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{
 		Resolvers: graph.NewResolver(db),
