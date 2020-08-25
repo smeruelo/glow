29bca6a232480da8f49a0b5cafa7b8873801cb77
Sonia Meruelo <smeruelo@gmail.com>
2020-08-25 09:47:57 +0200

    Add achievements to storage interface

--- storage/storage.go
+++ storage/storage.go
@@ -11,4 +11,11 @@ type Store interface {
 	GetUserProjects(uID string) ([]model.Project, error)
 	DeleteProject(pID, uID string) error
 	UpdateProject(pID string, np model.NewProject) (model.Project, error)
+
+	CreateAchievement(a model.Achievement) error
+	GetAchievement(aID string) (model.Achievement, error)
+	GetProjectAchievements(pID string) ([]model.Achievement, error)
+	GetUserAchievements(uID string) ([]model.Achievement, error)
+	UpdateAchievement(aID, newData model.AchievementData) (model.Achievement, error)
+	DeleteAchievement(aID, pID string) error
 }
